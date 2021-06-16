902716fcc24fb29a106581d27d8a79254c64b1e9
TolmaSh <m0v4anua@gmail.com>
2021-06-16 19:30:08 +0300

    Feat: add comments for all sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,9 @@
-const swiper = new Swiper('.swiper-container', {
-        slidesPerView: 1,
-        spaceBetween: 30,
-        
-        
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+function test(a, b) {
+    return a + b
+}
+
+console.log(test(5, 2));
+console.log(test(2, 4));
+console.log(test(9, 1));
 
 //# sourceMappingURL=script.js.map
