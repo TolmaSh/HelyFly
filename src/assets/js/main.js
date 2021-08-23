28ded0207a7781ec673945cc260151a3d7a27481
TolmaSh <m0v4anua@gmail.com>
2021-08-23 18:13:05 +0300

    Feat: add js for gallery btn

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,18 @@
+const gallery = document.querySelectorAll(".gallery--hide");
+const showMore = document.querySelector(".gallery__btn");
 
+showMore.addEventListener('click', () => {
+    galleryHide()
+    showMoreBtn()
+    
+})
+
+function galleryHide() {
+    gallery.forEach((photo) => {
+        photo.classList.toggle('gallery--hide');
+    })
+}
+function showMoreBtn() {
+    showMore.classList.toggle('btn--show');
+    
+}
